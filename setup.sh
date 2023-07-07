33da39bb12f602e3e6b2e173b4e1cd16dc7bd396
namickey <teethdai@gmail.com>
2023-07-08 06:56:05 +0900

    mod

--- setup.sh
+++ setup.sh
@@ -11,8 +11,14 @@ sudo yum -y install git
 # clone
 sudo -u ec2-user git clone https://github.com/namickey/spring-boot2-train.git
 
-# cat
-#sudo -u ec2-user cat /home/ec2-user/spring-boot2-train/setup-ec2.sh > /home/ec2-user/log.txt
+# service
+sudo cp /home/ec2-user/spring-boot2-train/run-app.service /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/run-app.service
+sudo chmod 664 /etc/systemd/system/run-app.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl start run-app.service
 
 # exec setup-ec2
-sudo -u ec2-user /bin/bash /home/ec2-user/spring-boot2-train/setup-ec2.sh
+#sudo -u ec2-user /bin/bash /home/ec2-user/spring-boot2-train/setup-ec2.sh
