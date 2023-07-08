635815883704a67c6fdac3994ea31eabc002dada
namickey <teethdai@gmail.com>
2023-07-08 09:36:45 +0900

    mod

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ sudo -u ec2-user git clone https://github.com/namickey/spring-boot2-train.git
 cp /home/ec2-user/spring-boot2-train/run-app.service /etc/systemd/system/
 chown root:root /etc/systemd/system/run-app.service
 chmod 664 /etc/systemd/system/run-app.service
+chmod 775 /home/ec2-user/spring-boot2-train/mvnw
 
 # service start
 systemctl daemon-reload
